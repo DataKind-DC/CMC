63c50ba2f29e06122bd17e79d76c27ea60d47df6
danbernstein <danbernstein94@gmail.com>
2020-04-30 08:02:25 -0400

    convert TODO to github issues

--- node_modules/react-dates/esm/utils/isPreviousDay.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-import isSameDay from './isSameDay';
-export default function isPreviousDay(a, b) {
-  if (!moment.isMoment(a) || !moment.isMoment(b)) return false;
-  var dayBefore = moment(a).subtract(1, 'day');
-  return isSameDay(dayBefore, b);
-}
--- node_modules/react-with-styles/test/ifReactHelpers.js
+++
@@ -1,9 +0,0 @@
-import ifReact from 'enzyme-adapter-react-helper/build/ifReact';
-
-export function describeIfReact(version, ...rest) {
-  return ifReact(version, describe, describe.skip)(...rest);
-}
-
-export default {
-  describeIfReact,
-};
